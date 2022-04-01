b2eaba3f6f212cc39a5015f2aa0358f6a319ad54
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-01 14:28:28 -0700

    I can't make it work, still working on it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 function numbers(inputMain) {
   const arrayNumbers = [];
-  for (let index = 0; index <= inputMain; index += 1) {
-    console.log(index);
-    arrayNumbers.push(index);
+  for (let i = 0; i <= inputMain; i += 1) {
+    console.log(i);
+    arrayNumbers.push(i);
   }
   return numbers;
 }
 
-function replace(input) {
-  let numbers = replace(input);
+function replace(inputMain) {
+  let numbers = replace(inputMain);
+  let modifyNumber = [];
+
   for (let i = 0; i <= numbers; i += 1) {
-    if (numbers[i]) {
+    array.forEach(function (inputMain) {
+      if (numbers[i] = 1) {
+        numbers[i] = "Beep!";
+
+      } else if (number[i] = 2) {
+        numbers[i] = "Boop!";
+      } else (number[i] = 3)
+
+      numbers[i] = "Won't you be my neighbor?"
+
 
     }
-  }
-}
+    modifyNumber.push(array[i]).replace(number[i])
+    )
+  };
 
+  return replace;
 
+};
