e09bdad4ef228713dcd416d8f190435ce7847b5a
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-01 16:55:25 -0700

    I couldn't finish the project.

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@ function numbers(inputMain) {
 };
 
 function newNumbers(inputMain) {
-  let arrayNumbers = newNumbers(inputMain);
-  let modifyNumber = [];
-  for (let i = 0; i >= 10; i += 1) {
+  let arrayNumbers = numbers(inputMain);
+
+  for (let i = 0; i > 9; i++) {
     // for each element that more than 10 we need to split it on new array to compare every element in side if there are (1 and 2) or (2 and 3) or (3 and 1) or (1 and 2 and 3). we replace the element with the word based if the greater number of there three.
 
+    let modifyNumber = [];
+    modifyNumber.push(arrayNumbers[i] += 1)
 
-  }
+
+
+
+  };
+  return modifyNumber;
 }
